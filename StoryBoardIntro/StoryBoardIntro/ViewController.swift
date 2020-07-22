10d02d272199c023a769e0502f98e50cd9bdfa49
emilyyeung66 <67671315+emilyyeung66@users.noreply.github.com>
2020-07-22 11:49:28 -0400

    KWK First Xcode Project

---
+++ StoryBoardIntro/StoryBoardIntro/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StoryBoardIntro
+//
+//  Created by Emily Yeung on 7/22/20.
+//  Copyright © 2020 KWK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print ("This is a print statement - testing testing!") 
+    }
+
+
+}
+
