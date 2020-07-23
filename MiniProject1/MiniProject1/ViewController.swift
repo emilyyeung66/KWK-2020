5864b5aab40d071a460a5e2624fdc93a2e561d69
emilyyeung66 <67671315+emilyyeung66@users.noreply.github.com>
2020-07-22 22:31:59 -0400

    MiniProject1

---
+++ MiniProject1/MiniProject1/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  MiniProject1
+//
+//  Created by Emily Yeung on 7/22/20.
+//  Copyright © 2020 KWK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func factsEmily(_ sender: UIButton) {
+        if textInputMode != nil {
+            let newTitle =  ["17 years old", "Born and raised in New York City", "Attending NYU"]
+            for _ in newTitle {
+            print (newTitle)
+        
+            }
+        
+            
+        }
+
+        
+    }
+   
+  
+    
+
+
+}
